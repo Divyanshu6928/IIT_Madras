3e3ba8c95013276d9b2dac36e0ab6adf9409ea0d
Divyanshu <lyricalv15@gmail.com>
2024-04-10 18:25:13 +0530

    OOPs

--- Object-Oriented Programming/Lecture programs/pr2.py
+++ Object-Oriented Programming/Lecture programs/pr2.py
@@ -1,4 +1,5 @@
 class Student:
+    count = 0
     def __init__(self,roll_no,name) :
         
         self.roll_no=roll_no
@@ -8,7 +9,11 @@ class Student:
         print(self.roll_no,self.name)
 
 s0=Student(0,'Bhuvanesh')
+Student.count+=1
 s0.display()
 
 s1=Student(1,'Harish')
+Student.count+=1
 s1.display()
+
+print(Student.count)
